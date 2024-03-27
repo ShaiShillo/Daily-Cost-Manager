61ee2b2d3b5155644d11c22dca1651b0f5c70065
ShaiShillo <shaishillo@gmail.com>
2024-03-27 12:05:25 +0200

    Third commit

--- app.js
+++ app.js
@@ -65,10 +65,5 @@ app.use(function(err, req, res, next) {
   res.render(`error`);
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 module.exports = app;
